d81ab0d3544926ea696bb3a23bf5850428c4c593
tk304 <tk304@pvsd.org>
2018-12-10 11:22:04 -0500

    sjrhv             List<Boolean> vals = new ArrayList<>();jbjnvt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,44 @@
 package com.company;
+import java.util.*;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static List<Boolean> StingToBool(String exp) throws java.lang.Exception
+    {
+        List<Boolean> vals = new ArrayList<>();
+        try
+        {
+            boolean a = true;
+            boolean b = true;
+            ScriptEngineManager mgr = new ScriptEngineManager();
+            ScriptEngine engine = mgr.getEngineByName("JavaScript");
+            vals.add(Boolean.valueOf((Boolean)engine.eval(exp.toLowerCase()));
+            a= false;
+            vals.add(Boolean.valueOf((Boolean)engine.eval(exp.toLowerCase()));
+            b = false;
+            vals.add(Boolean.valueOf((Boolean)engine.eval(exp.toLowerCase()));
+            a = true;
+            vals.add(Boolean.valueOf((Boolean)engine.eval(exp.toLowerCase()));
+
+
+        }
+        catch (Exception x)
+        {
+            System.out.println("Error parsing string.");
+        }
+        return vals;
+    }
+
+    public static void main(String[] args) throws java.lang.Exception {
+
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine().toLowerCase();
+        if (input != null || !input.equals(""))
+        {
+
+        }
     }
 }
